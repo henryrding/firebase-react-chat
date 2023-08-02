e54b02911691c746b5958ec1b895f95e13ff2b5d
Henry Ding <dinghenryr@gmail.com>
2023-08-02 22:32:44 +0000

    updated App.js, NavBar.js, and Welcome.js with Google authentication.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import NavBar from './components/NavBar';
 import ChatBox from './components/ChatBox';
 import Welcome from './components/Welcome';
-import React, { useState } from 'react';
+import React from 'react';
+import { auth } from './firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 function App() {
   // eslint-disable-next-line no-unused-vars
-  const [user, setUser] = useState(false);
+  const [user] = useAuthState(auth);
 
   return (
     <div className="App">
