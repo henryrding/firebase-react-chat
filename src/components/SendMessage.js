f7399bb4e3f4ab6013f26f8f5c850a5055dadb38
Henry Ding <dinghenryr@gmail.com>
2023-08-02 22:49:04 +0000

    updated SendMessage.js to send and store messages in firebase.

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { auth, db } from '../firebase';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
 
 const SendMessage = () => {
+  const [message, setMessage] = useState('');
+
+  const sendMessage = async (event) => {
+    event.preventDefault();
+    if (message.trim() === '') {
+      alert('Enter valid message');
+      return;
+    }
+    const { uid, displayName, photoURL } = auth.currentUser;
+    await addDoc(collection(db, 'messages'), {
+      text: message,
+      name: displayName,
+      avatar: photoURL,
+      createdAt: serverTimestamp(),
+      uid,
+    });
+    setMessage('');
+  };
+
   return (
-    <form className="send-message">
+    <form onSubmit={(event) => sendMessage(event)} className="send-message">
       <label htmlFor="messageInput" hidden>
         Enter Message
       </label>
@@ -11,7 +32,9 @@ const SendMessage = () => {
         name="messageInput"
         type="text"
         className="form-input__input"
-        placeholder="type message..."
+        placeholder="type a message..."
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
       />
       <button type="submit">Send</button>
     </form>
